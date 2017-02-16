82c38ee9f239021fd182f0d119a3e40de3552c69
Wes Duff <wduff@healthgrades.com>
2017-02-16 16:02:06 -0700

    Update

--- app.js
+++ app.js
@@ -35,14 +35,13 @@ if (cluster.isMaster) {
     var ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
     var snsTopic =  process.env.NEW_SIGNUP_TOPIC;
     var app = express();
-
     app.set('view engine', 'ejs');
     app.set('views', __dirname + '/views');
     app.use(bodyParser.urlencoded({extended:false}));
-
+     app.use(express.static('static'));
     app.get('/', function(req, res) {
         res.render('index', {
-            static_path: 'static',
+            static_path: '',
             theme: process.env.THEME || 'flatly',
             flask_debug: process.env.FLASK_DEBUG || 'false'
         });
