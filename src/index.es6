2c42a4ea4c36909ebe8ce871f45a0634ba1bba29
Wes Duff <wduff@healthgrades.com>
2017-02-17 15:47:07 -0700

    bootstrap and jquery added - working

--- src/index.es6
+++ src/index.es6
@@ -4,9 +4,9 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 //import './styles/styles.css'; //webpack can import css files too!
-import '../node_modules/jquery/dist/jquery.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../css/base.css'
+import '../css/base.css';
+
 
 render (
     <Router history={browserHistory} routes={routes} />,
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
                 loader: 'babel-loader'
             },
             { test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$/, loader: "url-loader" },
+            { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' }
           /*  { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] },
             { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url?limit=10000' },
             { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' }*/
