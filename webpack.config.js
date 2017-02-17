086c4d9b77ba0aa7c16b3f347b17ee7756c9350c
Wes Duff <wduff@healthgrades.com>
2017-02-17 14:31:46 -0700

    adding bootstrap and jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 module.exports = {
     entry: path.resolve(__dirname, 'src/index'),
     output: {
@@ -10,14 +11,30 @@ module.exports = {
     module: {
         
         loaders: [
-            
+            { 
+                test: /\.css$/, 
+                loader: "style-loader!css-loader" 
+            },
             {
                 test: /\.es6$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
-            }
+            },
+            { test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$/, loader: "url-loader" },
+          /*  { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] },
+            { test: /\.(woff2?|ttf|eot|svg)$/, loader: 'url?limit=10000' },
+            { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' }*/
+        ],
+        plugins: [
+            new webpack.ProvidePlugin({
+                jQuery: 'jquery',
+                $: 'jquery',
+                jquery: 'jquery'
+            })
         ]
     },
+    
+
     
     resolve: {
         extensions: ['', '.js', '.es6']
